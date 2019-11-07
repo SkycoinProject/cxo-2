2d8e26533336c49fdce74f663d8295b0d36ca9b4
LjubomirSN <ljubomir@blue-chang.com>
2019-11-07 22:01:03 +0100

    Cli autocomplete
    * Generated scripts for bash and zsh
    * Updated install script to add autocomplete
    scripts depending on shell and removed
    some flags due to errors during install

--- install.sh
+++ install.sh
@@ -1,6 +1,22 @@
-#!/usr/bin/env bash
-
-set -e -o pipefail
+#!/bin/bash
 
 go build -o "$GOPATH/bin/cxo-node" "$GOPATH/src/github.com/SkycoinPro/cxo-2-node/cmd/node"
 go build -o "$GOPATH/bin/cxo-node-cli" "$GOPATH/src/github.com/SkycoinPro/cxo-2-node/cmd/cli"
+
+if [ -f "$HOME/.bashrc" ]; then
+    echo "Installing cli completion for bash..."
+    cp "$GOPATH/src/github.com/SkycoinPro/cxo-2-node/cmd/cli/bash_completion.sh" "$HOME/.cxo-node-cli-completion.bash"
+
+    if ! grep -Fxq "source ~/.cxo-node-cli-completion.bash" "$HOME/.bashrc" ; then
+        echo "source ~/.cxo-node-cli-completion.bash" >> "$HOME/.bashrc"
+    fi
+fi
+
+if [ -f "$HOME/.zshrc" ]; then
+    echo "Installing cli completion for zsh..."
+    cp "$GOPATH/src/github.com/SkycoinPro/cxo-2-node/cmd/cli/zsh_completion.zsh" "$HOME/.cxo-node-cli-completion.zsh"
+
+    if ! grep -Fxq "source ~/.cxo-node-cli-completion.zsh" "$HOME/.zshrc" ; then
+        echo "source ~/.cxo-node-cli-completion.zsh" >> "$HOME/.zshrc"
+    fi
+fi
