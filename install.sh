9aaa506e923d26a28adeffbf559a04f037386bba
Srdjan Radulovic <srdjan@software-nation.com>
2019-11-07 07:08:18 +0100

    Modify build script
    * Remove dot from build script definition
    * Add local playground area for helper scripts

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 set -e -o pipefail
 
-go build -o "$GOPATH/bin/cxo-node ." "$GOPATH/src/github.com/SkycoinPro/cxo-2-node/cmd/node"
-go build -o "$GOPATH/bin/cxo-node-cli ." "$GOPATH/src/github.com/SkycoinPro/cxo-2-node/cmd/cli"
+go build -o "$GOPATH/bin/cxo-node" "$GOPATH/src/github.com/SkycoinPro/cxo-2-node/cmd/node"
+go build -o "$GOPATH/bin/cxo-node-cli" "$GOPATH/src/github.com/SkycoinPro/cxo-2-node/cmd/cli"
