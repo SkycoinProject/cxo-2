7676b0ae8daf722188fb3bca222a7573d6e9f6df
Marko Tagliavia <marko@blue-chang.com>
2019-12-20 18:15:08 +0100

    File update handled

--- integration/test-runner.go
+++ integration/test-runner.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"os/exec"
 	"time"
+	"strings"
 )
 
 const (
@@ -56,6 +57,12 @@ func waitOnNodeToBeUp() {
 func runCommands(conf config) {
 	for _, command := range conf.Commands {
 		if len(command.Actions) > 0 {
+			if command.Actions[0] == "publish" {
+				newContent := readContent(command.Actions[1])
+				newPath := cutVersionPart(command.Actions[1])
+				command.Actions[1] = newPath
+				updateFileContent(newPath, newContent)
+			}
 			fmt.Println("running commands: cxo-node-cli ", command.Actions)
 			cmd := exec.Command("cxo-node-cli", command.Actions...)
 			if err := cmd.Run(); err != nil {
@@ -72,6 +79,50 @@ func keepRunningOnEmpty() {
 	for {
 		time.Sleep(time.Second)
 	}
+}
+
+func updateFileContent(path, content string){
+	file, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE, 0777)
+
+	if err != nil {
+		log.Fatalf("Failed opening file: %s", err)
+	}
+	defer file.Close()
+
+	err = file.Truncate(0)
+	if err != nil {
+		log.Fatalf("Failed truncete of file: %s", err)
+	}
+
+	_, err = file.WriteString(content)
+	if err != nil {
+		log.Fatalf("Failed writing to file: %s", err)
+	}
+}
+
+func cutVersionPart(inpath string) string{
+	if strings.Contains(inpath, "-version") {
+		splitedCommands := strings.Split(inpath, "-version")
+		return splitedCommands[0] + ".txt"
+	}
+
+	return inpath
+}
+
+func readContent(path string) string{
+	file, err := os.Open(path)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+	b, err := ioutil.ReadAll(file)
+	if err != nil {
+        log.Fatal(err)
+		return ""
+	}
+
+	return string(b)
 }
 
 type config struct {
