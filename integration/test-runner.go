4d6cf6884433853c77e1efa416ca658c0c9cea56
Marko Tagliavia <marko@blue-chang.com>
2019-12-21 14:03:20 +0100

    Directory update support

--- integration/test-runner.go
+++ integration/test-runner.go
@@ -3,10 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
-	"os/exec"
 	"strings"
 	"time"
 )
@@ -48,13 +48,21 @@ func waitOnNodeToBeUp() {
 func runCommands(conf config) {
 	for _, command := range conf.Commands {
 		if len(command.Actions) > 0 {
-			// TODO (marko) update with the latest example including directory
-			// if command.Actions[0] == "publish" {
-			// 	newContent := readContent(command.Actions[1])
-			// 	newPath := cutVersionPart(command.Actions[1])
-			// 	command.Actions[1] = newPath
-			// 	updateFileContent(newPath, newContent)
-			// }
+			if command.Actions[0] == "publish" && strings.Contains(command.Actions[1], "-version") {
+				newPath := cutVersionPart(command.Actions[1])
+				if strings.Contains(newPath, ".txt") {	//File
+					newContent := readContent(newPath)
+					updateFileContent(newPath, newContent)
+				} else {	//Directory
+					removeContents(newPath)
+					err := copyFolder(command.Actions[1], newPath)
+					if err != nil {
+						log.Fatal(err)
+					}
+				}
+
+				command.Actions[1] = newPath
+			}
 			fmt.Println("running commands: cxo-node-cli ", command.Actions)
 			cmd := exec.Command("cxo-node-cli", command.Actions...)
 			if err := cmd.Run(); err != nil {
@@ -62,7 +70,7 @@ func runCommands(conf config) {
 			}
 		}
 
-		fmt.Printf("sleeping for %v seconds", command.SleepInSeconds)
+		fmt.Printf("sleeping for %v seconds\n", command.SleepInSeconds)
 		time.Sleep(time.Duration(command.SleepInSeconds) * time.Second)
 	}
 }
@@ -87,12 +95,12 @@ func updateFileContent(path, content string) {
 }
 
 func cutVersionPart(inpath string) string {
-	if strings.Contains(inpath, "-version") {
-		splitedCommands := strings.Split(inpath, "-version")
+	splitedCommands := strings.Split(inpath, "-version")
+	if strings.Contains(inpath, ".txt") {
 		return splitedCommands[0] + ".txt"
 	}
 
-	return inpath
+	return splitedCommands[0]
 }
 
 func readContent(path string) string {
@@ -109,6 +117,81 @@ func readContent(path string) string {
 	}
 
 	return string(b)
+}
+
+func removeContents(dir string) {
+	dir = dir[1:]
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		os.Mkdir(dir, os.ModeDir)
+	}
+
+    // Open the directory and read all its files.
+    dirRead, _ := os.Open(dir)
+    dirFiles, _ := dirRead.Readdir(0)
+
+    // Loop over the directory's files.
+    for index := range(dirFiles) {
+        fileHere := dirFiles[index]
+
+        // Get name of file and its full path.
+        nameHere := fileHere.Name()
+        fullPath := dir + nameHere
+
+        // Remove the file.
+        os.Remove(fullPath)
+    }
+}
+
+func copyFolder(source string, dest string) (err error) {
+	source = source[1:]
+	dest = dest[1:]
+	directory, _ := os.Open(source)
+
+	objects, err := directory.Readdir(-1)
+
+	for _, obj := range objects {
+		sourcefilepointer := source + "/" + obj.Name()
+		destinationfilepointer := dest + "/" + obj.Name()
+
+		if obj.IsDir() {
+			err = copyFolder(sourcefilepointer, destinationfilepointer)
+			if err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			err = copyFile(sourcefilepointer, destinationfilepointer)
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
+	return
+}
+
+func copyFile(source string, dest string) (err error) {
+	sourcefile, err := os.Open(source)
+	if err != nil {
+		return err
+	}
+
+	defer sourcefile.Close()
+
+	destfile, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+
+	defer destfile.Close()
+
+	_, err = io.Copy(destfile, sourcefile)
+	if err == nil {
+		sourceinfo, err := os.Stat(source)
+		if err != nil {
+			err = os.Chmod(dest, sourceinfo.Mode())
+		}
+	}
+
+	return
 }
 
 type config struct {
